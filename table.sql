53e8124f564a4eae1c6c28ae57b8ec19a7ced126
jzmnmiller40 <jzmnmiller40@knights.ucf.edu>
2022-04-28 03:03:11 -0400

    Created Table
    Creating table for Database

---
+++ table.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE `portfolio`;
+USE `portfolio`;
+
+CREATE TABLE `users`(
+
+`id` int(111) AUTO_INCREMENT,
+`username` varchar(255),
+`password` varchar(255),
+PRIMARY KEY(`id`)
+);
+
